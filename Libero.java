3ef04cab532b6fdd708e6d8722f44f60bec9ffe4
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-15 18:55:49 -0600

    Update Libero.java

--- Libero.java
+++ Libero.java
@@ -1,3 +1,17 @@
-public class Libero {
-    
+public class Libero extends Jugador {
+    private int recibos;
+
+    public Libero(String nombre, String pais, int errores, int aces, int total_servicios, float efectividad,
+            int recibos) {
+        super(nombre, pais, errores, aces, total_servicios, efectividad);
+        this.recibos = recibos;
+    }
+
+    public int getRecibos() {
+        return recibos;
+    }
+
+    public void setRecibos(int recibos) {
+        this.recibos = recibos;
+    }
 }
