ca1e43a8535a43b6a1e4e754432e6b2cb558be8e
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-15 18:55:53 -0600

    Update Pasador.java

--- Pasador.java
+++ Pasador.java
@@ -1,3 +1,24 @@
-public class Pasador {
+public class Pasador extends Jugador {
+    private int pases;
+    private int fintas;
+
+    public Pasador(String nombre, String pais, int errores, int aces, int total_servicios, float efectividad,
+            int pases, int fintas) {
+        super(nombre, pais, errores, aces, total_servicios, efectividad);
+        this.pases = pases;
+        this.fintas = fintas;
+    }
     
+    public int getPases() {
+        return pases;
+    }
+    public void setPases(int pases) {
+        this.pases = pases;
+    }
+    public int getFintas() {
+        return fintas;
+    }
+    public void setFintas(int fintas) {
+        this.fintas = fintas;
+    }
 }
