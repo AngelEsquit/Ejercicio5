d6cbbfecb2c072368fcce9cdf88584040782e229
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-15 19:02:19 -0600

    Update Opuesto.java

--- Opuesto.java
+++ Opuesto.java
@@ -1,3 +1,35 @@
 public class Opuesto {
+    private int ataques;
+    private int bloqueos_efectivos;
+    private int bloqueos_fallidos;
+
+    public Opuesto(int ataques, int bloqueos_efectivos, int bloqueos_fallidos) {
+        this.ataques = ataques;
+        this.bloqueos_efectivos = bloqueos_efectivos;
+        this.bloqueos_fallidos = bloqueos_fallidos;
+    }
+
+    public int getAtaques() {
+        return ataques;
+    }
+
+    public void setAtaques(int ataques) {
+        this.ataques = ataques;
+    }
+
+    public int getBloqueos_efectivos() {
+        return bloqueos_efectivos;
+    }
+
+    public void setBloqueos_efectivos(int bloqueos_efectivos) {
+        this.bloqueos_efectivos = bloqueos_efectivos;
+    }
+
+    public int getBloqueos_fallidos() {
+        return bloqueos_fallidos;
+    }
     
+    public void setBloqueos_fallidos(int bloqueos_fallidos) {
+        this.bloqueos_fallidos = bloqueos_fallidos;
+    }
 }
