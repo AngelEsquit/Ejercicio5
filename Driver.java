1f4e4f8d6fd8a4b95ea5b5037b51267f8cbc4d9d
AngelEsquit <esq23221@uvg.edu.gt>
2023-10-19 17:07:07 -0600

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Driver {
@@ -31,6 +32,13 @@ public class Driver {
         int fintas = 0;
         int recibos = 0;
         float efectividad = 1;
+
+
+        Jugador jugador;
+        Libero libero;
+        Opuesto opuesto;
+        Auxiliar auxiliar;
+        Pasador pasador;
         
         // Variable para saltar la primera fila de encabezados
         boolean primera_fila = true;
@@ -87,13 +95,16 @@ public class Driver {
                             case 11: // Recibos
                                 recibos = Integer.parseInt(datos);
                                 break;
+                            case 12: // Efectividad
+                                efectividad = Integer.parseInt(datos);
+                                break;
                             default:
                                 break;
                         }
                     }
                 }
 
-                switch (posicion.toLowerCase()) { // 
+                switch (posicion.toLowerCase()) { // Crear a los jugadores del CSV
                     case "auxiliar":
                         efectividad = ((((ataques + bloqueos_efectivos - bloqueos_fallidos - errores) * 100) / (ataques + bloqueos_efectivos + bloqueos_fallidos + errores)) + (aces * 100) / total_servicios);
                         jugadores.add(new Auxiliar(nombre, pais, errores, aces, total_servicios, efectividad, ataques, bloqueos_efectivos, bloqueos_fallidos));
@@ -113,7 +124,7 @@ public class Driver {
                 }
 
                 Comparator<Jugador> comparadorEfectividad = Comparator.comparing(Jugador::getEfectividad);
-                Collections.sort(jugadores, Collections.reverseOrder(comparadorEfectividad));
+                Collections.sort(jugadores, Collections.reverseOrder(comparadorEfectividad)); // Ordenar la lista de jugadores por efectividad
                 System.out.println(); // Salto de línea para cada fila
                 conteo = -1;
             }
@@ -138,11 +149,55 @@ public class Driver {
                     listaJugadores(jugadores);
                     break;
                 case 2: // Mejores 3 líberos
+                    mejoresLiberos(jugadores);
                     break;
                 case 3: // Cantidad de pasadores con más de un 80% de efectividad
-                    System.out.println("// Lógica para la opción 3");
+                    cantidadPasadores80(jugadores);
                     break;
-                case 4: // Salir
+                case 4: // Inscribir un jugador
+                    posicion = posicionJugador(scanner);
+                    jugador = preguntasDatos(scanner, posicion);
+                    jugadores.add(jugador);
+                    break;
+                case 5: // Salir
+                    String csvFilePath2 = "datos.csv"; // Ruta del archivo CSV
+
+                    try (FileWriter writer = new FileWriter(csvFilePath2)) {
+                        writer.append("nombre;posicion;pais;errores;aces;total_servicios;ataques;bloqueos_efectivos;bloqueos_fallidos;pases;fintas;recibos;efectividad\n"); // Escribir encabezados
+
+                        for (Jugador jugadorG : jugadores) {
+                            if (jugadorG instanceof Libero) {
+                                libero = (Libero) jugadorG;
+                                writer.append(libero.getNombre() + ";libero;" + libero.getPais() + ";" + libero.getErrores() + ";" + libero.getAces() + ";" + libero.getTotal_servicios() + ";-;-;-;-;-;" + libero.getRecibos() + ";" + libero.getEfectividad() + "\n");
+                            }
+
+                            else if (jugadorG instanceof Opuesto) {
+                                opuesto = (Opuesto) jugadorG;
+                                writer.append(opuesto.getNombre() + ";opuesto;" + opuesto.getPais() + ";" + opuesto.getErrores() + ";" + opuesto.getAces() + ";" + opuesto.getTotal_servicios() + ";" + opuesto.getAtaques()+ ";" + opuesto.getBloqueos_efectivos() + ";" + opuesto.getBloqueos_fallidos() + ";-;-;-;" + opuesto.getEfectividad() + "\n");
+                            }
+
+                            else if (jugadorG instanceof Pasador) {
+                                pasador = (Pasador) jugadorG;
+                                writer.append(pasador.getNombre() + ";pasador;" + pasador.getPais() + ";" + pasador.getErrores() + ";" + pasador.getAces() + ";" + pasador.getTotal_servicios() + ";-;-;-;" + pasador.getPases() + ";" + pasador.getFintas() + ";-;" + pasador.getEfectividad() + "\n");
+                            }
+
+                            else if (jugadorG instanceof Auxiliar) {
+                                auxiliar = (Auxiliar) jugadorG;
+                                writer.append(auxiliar.getNombre() + ";auxiliar;" + auxiliar.getPais() + ";" + auxiliar.getErrores() + ";" + auxiliar.getAces() + ";" + auxiliar.getTotal_servicios() + ";" + auxiliar.getAtaques()+ ";" + auxiliar.getBloqueos_efectivos() + ";" + auxiliar.getBloqueos_fallidos() + ";-;-;-;" + auxiliar.getEfectividad() + "\n");
+                            }
+                        }
+
+                        writer.append("Juan,25,Barcelona\n");
+                        writer.append("María,30,Madrid\n");
+                        writer.append("Pedro,22,Valencia\n");
+            
+                        System.out.println("Datos guardados en " + csvFilePath2);
+                    }
+                    
+                    catch (IOException e) {
+                        System.err.println("Error al guardar el archivo CSV: " + e.getMessage());
+                    }
+
                     salir = false;
                     System.out.println("Hasta pronto :)");
                     break;
@@ -167,7 +222,8 @@ public class Driver {
         System.out.println("1: Mostrar lista de jugadores inscritos");
         System.out.println("2: Mostrar los mejores líberos");
         System.out.println("3: Mostrar la cantidad de pasadores con más de un 80% de efectividad");
-        System.out.println("4: Salir");
+        System.out.println("4: Inscribir un nuevo jugador");
+        System.out.println("5: Salir");
         System.out.println("");
     }
 
@@ -181,14 +237,334 @@ public class Driver {
         }
     }
 
-    public static void mejoresJugadores(ArrayList<Jugador> jugadores) {
+    public static void mejoresLiberos(ArrayList<Jugador> jugadores) {
         int n = 1;
         System.out.println("");
         for (Jugador jugador : jugadores) {
             if (jugador instanceof Libero) {
-                System.out.println(n + ": " + jugador.toString());
-                n += 1;
+                if (n < 4) {
+                    System.out.println(n + ": " + jugador.toString());
+                    n += 1;
+                }
             }
         }
+    }
+
+    public static void cantidadPasadores80(ArrayList<Jugador> jugadores) {
+        int n = 0;
+        System.out.println("");
+        for (Jugador jugador : jugadores) {
+            if (jugador instanceof Pasador) {
+                if (jugador.getEfectividad() > 80) {
+                    n += 1;
+                }
+            }
+        }
+
+        if (n == 1) {
+            System.out.println("Hay "+ n + " pasador con efectividad mayor a 80%");
+        }
+
+        else {
+            System.out.println("Hay "+ n + " pasadores con efectividad mayor a 80%");
+        }
+    }
+
+    public static Jugador preguntasDatos(Scanner scanner, String posicion) {
+        String nombre = "";
+        String posicionN = "";
+        String pais = "";
+        int errores = 0;
+        int aces = 0;
+        int total_servicios = 0;
+        int ataques = 0;
+        int bloqueos_efectivos = 0;
+        int bloqueos_fallidos = 0;
+        int pases = 0;
+        int fintas = 0;
+        int recibos = 0;
+        float efectividad = 1;
+        Jugador jugador = null;
+        
+        switch (posicion.toLowerCase()) {
+            case "libero":
+                nombre = nombreJugador(scanner);
+                posicionN = "libero";
+                pais = paisJugador(scanner);
+                errores = erroresJugador(scanner);
+                aces = acesJugador(scanner);
+                total_servicios = totalServiciosJugador(scanner);
+                recibos = recibosJugador(scanner);
+                efectividad = ((((recibos - errores) * 100) / (recibos + errores)) + (aces * 100) / total_servicios);
+                jugador = new Libero(nombre, pais, errores, aces, total_servicios, efectividad, recibos);
+                break;
+            case "opuesto":
+                nombre = nombreJugador(scanner);
+                posicionN = "opuesto";
+                pais = paisJugador(scanner);
+                errores = erroresJugador(scanner);
+                aces = acesJugador(scanner);
+                total_servicios = totalServiciosJugador(scanner);
+                ataques = ataquesJugador(scanner);
+                bloqueos_efectivos = bloqueosEfectivosJugador(scanner);
+                bloqueos_fallidos = bloqueosFallidosJugador(scanner);
+                efectividad = ((((ataques + bloqueos_efectivos - bloqueos_fallidos - errores) * 100) / (ataques + bloqueos_efectivos + bloqueos_fallidos + errores)) + (aces * 100) / total_servicios);
+                jugador = new Opuesto(nombre, pais, errores, aces, total_servicios, efectividad, ataques, bloqueos_efectivos, bloqueos_fallidos);
+                break;
+            case "pasador":
+                nombre = nombreJugador(scanner);
+                posicionN = "pasador";
+                pais = paisJugador(scanner);
+                errores = erroresJugador(scanner);
+                aces = acesJugador(scanner);
+                total_servicios = totalServiciosJugador(scanner);
+                pases = pasesJugador(scanner);
+                fintas = fintasJugador(scanner);
+                efectividad = ((((pases + fintas - errores) * 100) / (pases + fintas + errores)) + (aces * 100) / total_servicios);
+                jugador = new Pasador(nombre, pais, errores, aces, total_servicios, efectividad, pases, fintas);
+                break;
+            case "auxiliar":
+                nombre = nombreJugador(scanner);
+                posicionN = "auxiliar";
+                pais = paisJugador(scanner);
+                errores = erroresJugador(scanner);
+                aces = acesJugador(scanner);
+                total_servicios = totalServiciosJugador(scanner);
+                ataques = ataquesJugador(scanner);
+                bloqueos_efectivos = bloqueosEfectivosJugador(scanner);
+                bloqueos_fallidos = bloqueosFallidosJugador(scanner);
+                efectividad = ((((ataques + bloqueos_efectivos - bloqueos_fallidos - errores) * 100) / (ataques + bloqueos_efectivos + bloqueos_fallidos + errores)) + (aces * 100) / total_servicios);
+                jugador = new Auxiliar(nombre, pais, errores, aces, total_servicios, efectividad, ataques, bloqueos_efectivos, bloqueos_fallidos);
+                break;
+            default:
+                System.out.println("Esa posición no exite");
+        }
+
+        return jugador;
+    }
+
+    public static String posicionJugador(Scanner scanner) {
+        System.out.println("Ingrese la posición del jugador");
+        String posicion = scanner.nextLine().toLowerCase();
+        return posicion;
+    }
+    
+    public static String nombreJugador(Scanner scanner) {
+        System.out.println("Ingrese el nombre del jugador");
+        String nombre = scanner.nextLine().toLowerCase();
+        return nombre;
+    }
+
+    public static String paisJugador(Scanner scanner) {
+        System.out.println("Ingrese el país del jugador");
+        String pais = scanner.nextLine().toLowerCase();
+        return pais;
+    }
+
+    public static int erroresJugador(Scanner scanner) {
+        int errores = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de errores del jugador");
+                errores = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+        
+        return errores;
+    }
+
+    public static int acesJugador(Scanner scanner) {
+        int aces = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de aces del jugador");
+                aces = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return aces;
+    }
+
+    public static int totalServiciosJugador(Scanner scanner) {
+        int total_servicios = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de servicios del jugador");
+                total_servicios = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return total_servicios;
+    }
+
+    public static int ataquesJugador(Scanner scanner) {
+        int ataques = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de ataques del jugador");
+                ataques = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return ataques;
+    }
+
+    public static int bloqueosEfectivosJugador(Scanner scanner) {
+        int bloqueos_efectivos = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de bloqueos efectivos del jugador");
+                bloqueos_efectivos = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return bloqueos_efectivos;
+    }
+
+    public static int bloqueosFallidosJugador(Scanner scanner) {
+        int bloqueos_fallidos = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de bloqueos fallidos del jugador");
+                bloqueos_fallidos = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return bloqueos_fallidos;
+    }
+
+    public static int pasesJugador(Scanner scanner) {
+        int pases = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de pases del jugador");
+                pases = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return pases;
+    }
+
+    public static int fintasJugador(Scanner scanner) {
+        int fintas = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de fintas del jugador");
+                fintas = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return fintas;
+    }
+
+    public static int recibosJugador(Scanner scanner) {
+        int recibos = -1;
+        boolean salir = true;
+
+        while (salir) {
+            try {
+                System.out.println("Ingrese la cantidad de recibos del jugador");
+                recibos = scanner.nextInt();
+                scanner.nextLine();
+            }
+            
+            catch (InputMismatchException e) {
+                System.out.println("");
+                System.out.println("Ingrese un número.");
+                scanner.nextLine();
+            }
+
+            salir = false;
+        }
+
+        return recibos;
     }
 }
